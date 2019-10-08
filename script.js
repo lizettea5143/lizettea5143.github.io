564b4665b06231828b32bfc7b1185a84dbdaadab
lizettea5143 <56265196+lizettea5143@users.noreply.github.com>
2019-10-08 11:17:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "lizette aguirre"
+let userName = "lizettea5143";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
